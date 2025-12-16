72654a1ae9e95db7a0cff6a1da27e9160a23e39f
LapTop <h18381603535@gmail.com>
2025-12-16 12:42:19 +0800

    commit message

--- log.h
+++ log.h
@@ -6,6 +6,10 @@
 #include<list>
 #include<sstream>   // std::stringstream
 #include<fstream>
+#include<vector>
+#include <iostream>
+#include <map>
+
 
 namespace sylar{
     // 日志事件
@@ -13,34 +17,70 @@ namespace sylar{
         public:
             typedef std::shared_ptr<LogEvent> ptr;
             LogEvent();
+
+            const char* getFile() const { return m_file; }
+            int32_t getLine() const { return m_line; }
+            uint32_t getElapse() const { return m_elapse; }
+            uint32_t getThreadId() const { return m_threadId; }
+            uint32_t getFiberId() const { return m_fiberId; }
+            uint64_t getTime() const { return m_time; }
+            const std::string &getContent() const { return m_content; }
+            // Loglevel::Level getLevel() const { return m_level; };
+
         private:
-            const char *m_file = nullptr;   // 文件名
-            uint32_t m_line = 0;             // 行号
+            const char* m_file = nullptr;   // 文件名
+            int32_t m_line = 0;             // 行号
             uint32_t m_elapse = 0;          // 程序启动开始到现在的毫秒数
             uint32_t m_threadId = 0;        // 线程id
             uint32_t m_fiberId = 0;         // 协程id
             uint64_t m_time = 0;            // 时间戳
-            std::string m_content;          //
+
+            std::string m_content;          // 消息体？
     };
 
     // 日志级别
     class LogLevel{
         public:
-            enum Level 
+            enum Level
             {
+                UNKNOW = 0,
                 DEBUG = 1,
                 INFO = 2,
                 WARN = 3,
                 ERROR = 4,
                 FATAL = 5
             };
+            // 把enum变量转换为字符串比如:Level::DEBUG -> "DEBUG"
+            static const char *ToString(LogLevel::Level Level);
+            LogLevel::Level getLevel();
     };
 
-    // 日志格式
+    // 日志格式器
     class LogFormatter{
         public:
             typedef std::shared_ptr<LogFormatter> ptr;
+            // Constructor
+            LogFormatter(const std::string &pattern);
+            // &t    %thread_id %m%n
             std::string format(LogEvent::ptr event);
+
+        private:
+            class FormatItem{
+                public:
+                    typedef std::shared_ptr<FormatItem> ptr;
+                    // 虚析构函数
+                    virtual ~FormatItem(){};
+                    // 纯虚函数，如果子类没有override，就只是一个声明
+                    // 输出到文件
+                    // 1. 使用 std::ostream& 作为父类，兼容 stringstream 和 ofstream
+                    // 2. 必须加 & (引用)，流不能拷贝
+                    virtual void format(LogLevel::Level level, LogEvent::ptr event) = 0;
+            };
+            void init();
+
+        private:
+            std::string m_pattern;
+            std::vector<FormatItem::ptr> m_items;
     };
 
     // 日志输出地
@@ -98,9 +138,11 @@ namespace sylar{
             typedef std::shared_ptr<FileLogAppender> ptr;
             FileLogAppender(const std::string &filename);
             void log(LogLevel::Level level, LogEvent::ptr event) override;
+            // 文件打开成功返回true
+            bool reopen();
 
         protected:
-            std::string m_name;
+            std::string m_filename;
             std::ofstream m_filestream;
     };
 }
--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include "log.h"
 
+
 namespace sylar{
     // 列表初始化的形式的构造函数
     Logger::Logger(const std::string& name) : m_name(name) {}
@@ -37,21 +38,192 @@ namespace sylar{
     void Logger::warn(LogEvent::ptr event){
         log(LogLevel::WARN, event);
     }
-    void Logger::error(LogEvent::ptr event){
-        log(LogLevel::ERROR, event);
-    }
+    void Logger::error(LogEvent::ptr event){ log(LogLevel::ERROR, event); }
+
     void Logger::fatal(LogEvent::ptr event){
         log(LogLevel::FATAL, event);
     }
 
-    FileLogAppender::FileLogAppender(const std::string filename): m_filename(filename) {
+    // 构造函数
+    FileLogAppender::FileLogAppender(const std::string& filename) :m_filename(filename){}
+    
 
+    bool FileLogAppender::reopen(){
+        if(m_filestream){
+            m_filestream.close();
+        }
+        m_filestream.open(m_filename);
+        // !!的作用是非0值转成1,0值还是0,转化成真正的bool值
+        return !!m_filestream;
     }
 
-    void FileLogAppender::log(LogLevel::Level level, LogEvent::ptr event){}
+    void FileLogAppender::log(LogLevel::Level level, LogEvent::ptr event){
+        // 当这条日志的等级 >= 设定的最低等级时才记录。
+         if(level >= m_level){
+            // 右边的文件通过<<传入了m_filestream代表的文件里
+             m_filestream << m_formatter->format(event);
+         }
+    }
 
     void StdoutLogAppender::log(LogLevel::Level level, LogEvent::ptr event){
-
+        if (level >= m_level)
+            std::cout << m_formatter->format(event);
     }
-    
-}
+
+    //
+    LogFormatter::LogFormatter(const std::string &pattern):m_pattern(pattern){}
+
+    std::string LogFormatter::format(LogEvent::ptr event){
+        std::stringstream ss;
+        for(auto& i: m_items)
+            i->format(ss, event);
+        return ss.str();
+    }
+
+    void LogFormatter::init(){
+        /*
+            %m 消息
+            %p 日志级别
+            %c 日志器名称
+            %d 日期时间，后面可跟一对括号指定时间格式，比如%d{%Y-%m-%d %H:%M:%S}，这里的格式字符与C语言strftime一致
+            %r 该日志器创建后的累计运行毫秒数
+            %f 文件名
+            %l 行号
+            %t 线程id
+            %F 协程id
+            %N 线程名称
+            %% 百分号
+            %T 制表符
+            %n 换行
+        */
+
+        // 临时存储解析出来的结构：<格式字符, 附加参数(如时间格式), 类型>
+        // 类型 0: 普通字符串, 1: 格式化项
+        // string format type
+        std::vector < std::tuple<std::string, std::string, int >> vec;
+        std::string nstr; // 暂存普通字符串
+
+        for (auto i = 0; i < m_pattern.size();++i){
+            // 如果不是%说明是一个普通字符
+            if(m_pattern[i] != '%'){
+                // 追加n m_pattern[i]
+                nstr.append(1, m_pattern[i]);
+                continue;
+            }
+        
+            // 如果是%，且后面还是%，说明是转义，输出一个真实的%
+            if((i+1) < m_pattern.size()){
+                if(m_pattern[i+1] == '%'){
+                    nstr.append(1, '%');
+                    continue;
+                }
+            }
+
+            // 真实的格式字符(%d.%m)。先把之前的普通字符串存下；
+            if(!nstr.empty()){
+                vec.push_back(std::make_tuple(nstr, "", 0));    // 类型0: 普通串
+                nstr.clear();
+            }
+
+            // 开始解释%及其后面的内容
+            // fmt_status: 0-解释格式字符(如d),1-解析大括号状态, 2-大括号解说
+            auto fmt_status = 0;
+            auto n = i + 1;
+            std::string str;    // 格式化内容(d,m)
+            std::string fmt;    // 括号内的参数(%Y-%m-%d)
+
+            auto fmt_begin = 0;
+
+            while (n < m_pattern.size())
+            {
+                // 如果遇到不是字母也不是括号，说明这个格式符解析完成了(比如空格)
+                if(!fmt_status and (!isalpha(m_pattern[n]) and m_pattern[n] != '{' and m_pattern[n] != '}')){
+                    str = m_pattern.substr(i + 1, n - i - 1);
+                    break;
+                }
+                // 如果是解释格式字符
+                if(fmt_status==0){
+                    if(m_pattern[n]=='{'){
+                        str = m_pattern.substr(i + 1, n - i - 1);
+                        fmt_status = 1; // 进入括号解析模式
+                        fmt_begin = n;
+                        ++n;
+                        continue;
+                    }
+                }else if(fmt_status == 1){
+                    if (m_pattern[n] == '}'){
+                        // 括号结束
+                        fmt = m_pattern.substr(fmt_begin + 1, n - fmt_begin - 1);
+                        fmt_status = 0;
+                        ++n;
+                        break;
+                    }
+                }
+                ++n;
+                // 如果只有%d 而没有 {}, 且遍历到了末尾
+                if(n == m_pattern.size()){
+                    if(str.empty()){
+                        str = m_pattern.substr(i + 1);
+                    }
+                }
+            }
+            if(fmt_status == 0){
+                if(!nstr.empty()){
+                    vec.push_back(std::make_tuple(str, fmt, 1));
+                    i = n - 1;
+                }else if(fmt_status == 1){
+                    std::cout << "pattern parse error: " << m_pattern << " - " << m_pattern.substr(i) << std::endl;
+                    vec.push_back(std::make_tuple("<<pattern_error>>", fmt, 0));
+                }
+            } 
+        }// for end
+
+        if(!nstr.empty()){
+            vec.push_back(std::make_tuple(nstr, "", 0));
+        }
+
+        /*
+            %m -- 消息体
+            %p -- level
+            %r -- 启动后的时间
+            %c -- 日志名称
+            %t -- 线程id
+            %n -- 换行
+            %d -- 时间
+            %f -- 文件名
+            %l -- 行号
+        */
+    } // init end
+
+    class MessageFormatItem : public LogFormatter::FormatItem{
+    public:
+        void format(std::ostream& os, LogEvent::ptr event) override{
+            os << event->getContent();
+        }
+    };
+
+    class LevelFormatItem : public LogFormatter::FormatItem{
+    public:
+        void format(std::ostream& os, LogEvent::ptr event) override{
+            os << LogLevel::ToString(event->getLevel());
+        }
+    };
+
+    const char* LogLevel::ToString(LogLevel::Level Level){
+        switch(Level){
+    #define XX(name)         \
+            case LogLevel::name: \
+                return #name;    \
+                break;
+            XX(DEBUG);
+            XX(INFO);
+            XX(WARN);
+            XX(ERROR);
+            XX(FATAL);
+    #undef XX   //define结束
+            default:
+                return "UNKNOW";
+            }
+    }
+
+} // namespace sylar 
